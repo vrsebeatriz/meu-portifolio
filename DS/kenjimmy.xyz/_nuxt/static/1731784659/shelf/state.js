598aa4d5956df21157a8c774e53aad7badf22bd1
Beatriz <anabaraujo009@gmail.com>
2026-05-02 00:49:41 -0300

    chore: ignore DS folder and remove from tracking

--- DS/kenjimmy.xyz/_nuxt/static/1731784659/shelf/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1731784659",layout:"default",error:null,state:{store:{}},serverRendered:a,routePath:"\u002Fshelf",config:{},colorMode:{preference:b,value:b,unknown:a}}}(true,"light"));
--- DS/kenjimmy.xyz/_nuxt/static/1731784659/shelf/synchronous-and-asynchronous-learning-methods/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1731784659",layout:"default",error:null,state:{store:{}},serverRendered:a,routePath:"\u002Fshelf\u002Fsynchronous-and-asynchronous-learning-methods",config:{},colorMode:{preference:b,value:b,unknown:a}}}(true,"light"));
